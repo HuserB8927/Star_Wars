833c2f8a1dcb93fccef721cd71de721aa167f829
Halász Benjamin <47182317+HuserB8927@users.noreply.github.com>
2021-09-29 22:45:28 +0200

    swiper iimplemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './component/login/login.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './component/login/login.component';
+import {NavbarComponent} from './component/navbar/navbar.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexModule} from "@angular/flex-layout";
-import { CharactersComponent } from './component/characters/characters.component';
-import { SimulationComponent } from './component/simulation/simulation.component';
+import {CharactersComponent} from './component/characters/characters.component';
+import {SimulationComponent} from './component/simulation/simulation.component';
+import {SwiperModule} from "swiper/angular";
 
 @NgModule({
   declarations: [
@@ -24,19 +25,21 @@ import { SimulationComponent } from './component/simulation/simulation.component
     CharactersComponent,
     SimulationComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        BrowserAnimationsModule,
-        FlexModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FlexModule,
+    SwiperModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/component/characters/characters.component.ts
+++ src/app/component/characters/characters.component.ts
@@ -3,6 +3,7 @@ import {CharacterListItemModel} from "../../model/characterListItem.model";
 import {CharacterService} from "../../service/character.service";
 import {Router} from "@angular/router";
 import {HttpErrorResponse} from "@angular/common/http";
+import {SwiperOptions} from "swiper";
 
 @Component({
   selector: 'app-characters',
@@ -15,9 +16,6 @@ export class CharactersComponent implements OnInit {
 
   constructor(private characterService: CharacterService,
               private router: Router) {
-  }
-
-  ngOnInit(): void {
     this.characterService.getCharacters().subscribe(
       resp => {
         this.characters = resp;
@@ -28,6 +26,9 @@ export class CharactersComponent implements OnInit {
     );
   }
 
+  ngOnInit(): void {
+  }
+
   private static handleError(error: HttpErrorResponse) {
     if (error.status === 400) {
       console.error({"error": "Nincs ApplicantId"});
@@ -35,4 +36,56 @@ export class CharactersComponent implements OnInit {
       console.error({"error": "Method Not Allowed"});
     }
   }
+
+    Images: Array<any> = [
+      {
+        src: "/assets/anakin.png",
+        alt: 'Image 1',
+      }, {
+        src: '/assets/boba.png',
+        alt: 'Image 2'
+      }, {
+        src: '/assets/grievous.png',
+        alt: 'Image 3'
+      }, {
+        src: '/assets/kenobi.png',
+        alt: 'Image 4'
+      }, {
+        src: '/assets/luke.png',
+        alt: 'Image 5'
+      }, {
+        src: '/assets/maul.png',
+        alt: 'Image 6'
+      }, {
+        src: '/assets/phasma.png',
+        alt: 'Image 6'
+      }, {
+        src: '/assets/rey.png',
+        alt: 'Image 6'
+      }, {
+        src: '/assets/solo.png',
+        alt: 'Image 6'
+      }, {
+        src: '/assets/stormtrooper.png',
+        alt: 'Image 6'
+      }, {
+        src: '/assets/vader.png',
+        alt: 'Image 6'
+      }, {
+        src: '/assets/yoda.png',
+        alt: 'Image 6'
+      }
+    ]
+
+  config: SwiperOptions = {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 30
+  };
 }
